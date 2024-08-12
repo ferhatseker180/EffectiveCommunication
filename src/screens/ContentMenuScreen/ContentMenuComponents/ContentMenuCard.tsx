84d9dd447a536729334a0afc1033ff0c81f04ee8
FERHAT <73959099+ferhatseker180@users.noreply.github.com>
2024-08-12 23:58:41 +0300

    ContentMenu Ekranı ayarlandı ve Body Language için ön deneme yapıldı.

--- src/screens/ContentMenuScreen/ContentMenuComponents/ContentMenuCard.tsx
+++ src/screens/ContentMenuScreen/ContentMenuComponents/ContentMenuCard.tsx
@@ -1,25 +1,36 @@
 import { View, TouchableOpacity, Image } from 'react-native';
-import React from 'react';
+import React, { useState } from 'react';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import styles from './ContentMenuCardStyle';
 import { Avatar, Button, Card, Text } from 'react-native-paper';
 
-const ContentMenuCard = () => {
+interface ContentMenuCardProps {
+  title: string;
+  contentImageSource: any;
+  onPress: () => void;
+}
+
+const ContentMenuCard = ({title, contentImageSource, onPress}: ContentMenuCardProps) => {
   const LeftContent = (props : any) => <Avatar.Icon {...props} icon="folder" />
+  const [imageError, setImageError] = useState(false);
+  const handleImageError = () => {
+    setImageError(true);
+  };
 
   return (
     <Card style={styles.card}>
       <View style={styles.cardContent}>
         <Card.Cover 
           style={styles.content_image} 
-          source={{ uri: 'https://picsum.photos/700' }} 
+          source={contentImageSource} 
+          onError={handleImageError}
         />
         <View style={styles.textContainer}>
-          <Text style={styles.cardTitle}>İhtişamlı Gösteren 15 Beden Dili Hareketini Öğrenmek İster Misin? İmajını Güçlendirme Zamanı </Text>
+          <Text style={styles.cardTitle}>{title} </Text>
         </View>
       </View>
       <Card.Actions>
-        <Button>Oku</Button>
+        <Button onPress={onPress}>Oku</Button>
       </Card.Actions>
     </Card>
   );
--- src/screens/ContentMenuScreen/ContentMenuScreen.tsx
+++ src/screens/ContentMenuScreen/ContentMenuScreen.tsx
@@ -1,14 +1,38 @@
-import { View, Text } from 'react-native'
+import { View, Text, FlatList } from 'react-native'
 import React from 'react'
 import ContentMenuCard from './ContentMenuComponents/ContentMenuCard'
+import data from '../../data/BodyLanguage.json';
+
+// Dinamik olmayan bir nesne ile image path'lerini eşleştirin
+const imageMap: { [key: string]: any } = {
+    "anxiety.png": require('../../data/images/anxiety.png'),
+    "couple.png": require('../../data/images/couple.png'),
+    "stress.png": require('../../data/images/stress.png'),
+  };
 
 const ContentMenuScreen = ({route, navigation} : any) => {
-    const {contentMenuItem} = route.params;
-  return (
-    <View>
-      <ContentMenuCard  /> 
-    </View>
-  )
+    const renderItem = ({ item }: any) => {
+        const imageSource = imageMap[item.imageSource] || require('../../data/images/no-camera.png');
+        
+        return (
+            <ContentMenuCard
+                title={item.title}
+                contentImageSource={imageSource} // Lokal image yolu
+                onPress={() => console.log('Press Button')}
+            />
+        );
+    };
+
+    return (
+        <View>
+            <FlatList
+                data={data}
+                renderItem={renderItem}
+                keyExtractor={(item) => item.id.toString()}
+            />
+        </View>
+    );
 }
+
 
 export default ContentMenuScreen
